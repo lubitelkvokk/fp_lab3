bb134c95740e5c3a391af8727cb972f18697019d
lubitelkvokk <pasha110.mikhailov@yandex.ru>
2024-10-23 14:17:31 +0300

    fast save

--- src/lab3.erl
+++ src/lab3.erl
@@ -6,12 +6,13 @@
 start() ->
     Pid_process_point = interpolation_process:spawn_process_point([{0.0, 0.0}, {1.571, 1}]),
     Pid_output = output:spawn_output_process(),
-    Pid_manager = manager:spawn_manage_algorithm_io_process(Pid_process_point, self(), Pid_output),
+    Pid_manager = manager:spawn_manage_algorithm_io_process(Pid_process_point, Pid_output),
     spawn(?MODULE, loop, [Pid_manager]).
 
 % Основной цикл, который ожидает ввода от пользователя
 loop(Pid_manage_process) ->
     io:format("Введите число (или 'stop' для завершения): "),
+    timer:sleep(50),
     % Считываем строку
     Input = io:get_line(""),
     % Убираем лишние пробелы и переводы строк
@@ -21,7 +22,7 @@ loop(Pid_manage_process) ->
             ok;
         TrimmedInput ->
             try
-                [X, Y] = re:split(TrimmedInput),
+                [X, Y] = re:split(TrimmedInput, " "),
                 {X_flaot, _} = string:to_float(X),
                 {Y_float, _} = string:to_float(Y),
                 Pid_manage_process ! {self(), {X_flaot, Y_float}},
